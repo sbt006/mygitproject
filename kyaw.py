91e215960c755d0246f217e2e9076a2a46388cce
MetroJP <ye-htet-kyaw@metrobooks.co.jp>
2023-09-12 18:17:02 +0900

    "Add.gitattributes file to enable Git Large File Storage (LFS) for all Python files."

--- kyaw.py
+++ kyaw.py
@@ -1,9 +1,3 @@
-#This is my python script own by kyaw
-
-#Updated by Nyan
-
-#Second Time updated by Nyan
-
-#Thank
-
-#Haha
+version https://git-lfs.github.com/spec/v1
+oid sha256:37e7b48533b9a27813ddb453a4155fad843d34d657693c4b2bd510fa8a67014e
+size 100
