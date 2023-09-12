de6b2325e41a7e1a6b54183f49db440908e2d58d
MetroJP <ye-htet-kyaw@metrobooks.co.jp>
2023-09-12 18:32:05 +0900

    updated by kyaw

--- kyaw.py
+++ kyaw.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37e7b48533b9a27813ddb453a4155fad843d34d657693c4b2bd510fa8a67014e
-size 100
+oid sha256:731674aac63a5c52e59818e9f17e02c7e11d8817e90917b48af7e2615213fc41
+size 115
